d718ae2b8c0f657efeabeeefff4f1b22d137960a
gayu2004 <47376843+gayu2004@users.noreply.github.com>
2019-02-06 00:04:33 -0800

    Create odd.java

---
+++ odd.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a;
+		Scanner s=new Scanner(System.in);
+		System.out.println("");
+		a=s.nextInt();
+		if(a%2==0)
+		{
+			System.out.println("even");
+		}
+		else
+		{
+			System.out.println("odd");
+		}
+	}
+}
