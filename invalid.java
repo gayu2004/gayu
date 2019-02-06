0bb6145ab8447cfb53b3aade3886bf518f9bb58e
gayu2004 <47376843+gayu2004@users.noreply.github.com>
2019-02-05 23:00:01 -0800

    Create invalid.java

---
+++ invalid.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a;
+		Scanner s=new Scanner(System.in);
+		System.out.println("");
+		a=s.nextInt();
+		if(a%2==0)
+		{
+			System.out.println("even");
+		}
+		else
+		{
+			System.out.println("invalid");
+		}
+	}
+}
