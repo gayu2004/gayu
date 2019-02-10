0c85cdbb317e4518ec7e36f228096215934ca092
gayu2004 <47376843+gayu2004@users.noreply.github.com>
2019-02-10 18:51:51 +0530

    Create n,k.java

---
+++ n,k.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	
+		int K,i=1 ;
+		
+        int sum=0;
+         Scanner s = new Scanner(System.in);
+		System.out.println("");
+       N=s.nextInt();
+       System.out.println("");
+        K=s.nextInt();
+        while(i<=k)
+        {
+            sum=sum+i;
+            i++;
+        }
+        System.out.println(+sum);
+	}
+}
+        
