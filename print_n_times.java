9fa5f061df1cb4d737e57ad65aa02f2b2061f09f
gayu2004 <47376843+gayu2004@users.noreply.github.com>
2019-02-10 15:48:30 +0530

    Create print_n_times.java

---
+++ print_n_times.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+  {
+	Scanner s=new Scanner(System.in);
+		int n;
+		System.out.println("");
+		n=s.nextInt();
+		for(int i=0;i<n;i++) 
+		{
+			System.out.println("Hello");
+			
+		}
+  }
+}
