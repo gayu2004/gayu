a9cd49851213c9b61ac2a8aa9101c8f17ea39dd4
gayu2004 <47376843+gayu2004@users.noreply.github.com>
2019-02-11 20:44:03 +0530

    Create oddtwofrst.java

---
+++ oddtwofrst.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int n,i,k;
+	Scanner s=new Scanner(System.in);
+	System.out.println("");
+	k=s.nextInt();
+		System.out.println("");
+		n=s.nextInt();
+		int arr[]=new int[1000];
+		
+		for (i=k;i<n;i++)
+		{
+			if (i % 2 != 0)
+			{
+				System.out.print(+i);
+			}
+	   }
+   }
+}
