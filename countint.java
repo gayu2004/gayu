bb47bd481bb1bfd22b28cdfa1bdb9267beb119ca
gayu2004 <47376843+gayu2004@users.noreply.github.com>
2019-02-11 19:03:31 +0530

    Create countint.java

---
+++ countint.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 Scanner s=new Scanner(System.in);
+		 int count=0,num;
+		 System.out.println("");
+		 num=s.nextInt();
+         while(num !=0)
+        {
+            num /= 10;
+            ++count;
+        }
+
+        System.out.println(+ count);
+	}
+}
