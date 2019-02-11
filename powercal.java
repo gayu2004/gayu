8b93dba95efaf80c523f0a4656641edb59a75738
gayu2004 <47376843+gayu2004@users.noreply.github.com>
2019-02-11 19:14:08 +0530

    Create powercal.java

---
+++ powercal.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner s=new Scanner(System.in);
+		int base,exponent;
+		System.out.println("");
+		base=s.nextInt();
+		System.out.println();
+		exponent=s.nextInt();
+        long result=1;
+        while (exponent!= 0)
+        {
+            result *= base;
+            --exponent;
+        }
+
+        System.out.println( + result);
+    }
+	}
