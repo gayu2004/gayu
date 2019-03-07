b5d3b04ee63ba8fa31a31786fae6c303a7dedde7
gayu2004 <47376843+gayu2004@users.noreply.github.com>
2019-03-07 21:25:26 +0530

    Create mulfrt5.java

---
+++ mulfrt5.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int mul=0;
+		int cn=1;
+		Scanner s=new Scanner(System.in);
+		int n;
+		n=s.nextInt();
+		do {
+			if(cn% n== 0) {
+				mul++;
+				System.out.print(" "+cn);
+			}
+			cn++;
+		} while(mul<5);
+	}
+}
