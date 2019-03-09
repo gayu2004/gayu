e2485c3f1905603c93e38e7bd287f8643d8b3f04
gayu2004 <47376843+gayu2004@users.noreply.github.com>
2019-03-09 20:12:38 +0530

    Update countstring.java

--- countstring.java
+++ countstring.java
@@ -3,19 +3,21 @@ import java.util.*;
 class Ideone
 
     {
-        public static void main(String arg[]){
+        public static void main(String arg[])
+        {
         int sum=0,l;
         Scanner s=new Scanner(System.in);
       
         String val=s.nextLine();
          l=val.length();
-            for(int i=0;i<l;i++){
+            for(int i=0;i<l;i++)
+            {
                 if(val.charAt(i)==' ')
                     {
                         continue;
                     }
                     sum++;
-                    }
+            }
                     System.out.println(sum);
-                }
+          }
     }
